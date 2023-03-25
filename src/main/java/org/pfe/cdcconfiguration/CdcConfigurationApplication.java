5d34fcf6c1af8a05ddda8215f60a2f78c1b9c696
Mohamed Amine Ben Salem <abdbsalem@gmail.com>
2023-03-25 01:22:05 +0100

    init project

---
+++ src/main/java/org/pfe/cdcconfiguration/CdcConfigurationApplication.java
@@ -0,0 +1,13 @@
+package org.pfe.cdcconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdcConfigurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CdcConfigurationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/pfe/cdcconfiguration/CdcConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pfe.cdcconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdcConfigurationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
